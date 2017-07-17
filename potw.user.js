c75ab6cc31396d04bd0b8a02464557ae909592c5
7Snails <collinb1129@gmail.com>
2017-07-17 12:30:42 -0400

    Update potw.user.js

--- potw.user.js
+++ potw.user.js
@@ -1,14 +1,13 @@
 // ==UserScript==
-// @name         Project of the Week
-// @namespace    http://scratch.mit.edu/users/7Snails/
-// @version      0.0.1
-// @description  Adds a new "featured" section to the home page
-// @author       @7Snails
-// @match        http://scratch.mit.edu/
-// @match        https://scratch.mit.edu/
-// @icon         http://tampermonkey.net/favicon.ico
-// @grant        none
+// @name           Site - Script
+// @description    ES6 script
+// @author         James Skinner <spiralx@gmail.com> (http://github.com/spiralx)
+// @namespace      http://spiralx.org/
+// @version        0.0.1
+// @icon           http://tampermonkey.net/favicon.ico
+// @match          *://scratch.mit.edu
+// @grant          none
+// @run-at         document-end
 // ==/UserScript==
 
-console.log("testing 1")
-console.log(document.getElementsByTagName("h4")[1].innerHTML);
+console.log("testing")
