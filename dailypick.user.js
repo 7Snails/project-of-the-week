4416ea93eb885ba989a227103b6fdbeb57242aba
7Snails <collinb1129@gmail.com>
2017-07-16 21:48:54 -0400

    Create dailypick.user.js

---
+++ dailypick.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         The Daily Pick
+// @namespace    http://scratch.mit.edu/users/7Snails/
+// @version      0.0.1
+// @description  Adds a new "featured" section to the home page
+// @author       @7Snails
+// @match        http://scratch.mit.edu/
+// @match        https://scratch.mit.edu/
+// @icon         http://tampermonkey.net/favicon.ico
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
